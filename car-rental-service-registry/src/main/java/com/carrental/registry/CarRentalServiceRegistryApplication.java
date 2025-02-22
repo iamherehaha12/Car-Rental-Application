fb75b3d25732cdfc9a3dd99c6f76b4a15ba6ad6a
SAYED ATIF ALI ABID ALI <aatif3321@gmail.com>
2025-02-22 12:04:08 +0530

    Discovery services

---
+++ car-rental-service-registry/src/main/java/com/carrental/registry/CarRentalServiceRegistryApplication.java
@@ -0,0 +1,15 @@
+package com.carrental.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class CarRentalServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalServiceRegistryApplication.class, args);
+	}
+
+}
---
+++ car-rental-service-registry/src/test/java/com/carrental/registry/CarRentalServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrental.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
